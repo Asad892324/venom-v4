8dc34fbf05a1cab543e2471e076c3dff8e39bd90
Asad892324 <nilofersalmani26@gmail.com>
2024-08-31 20:11:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("6531277185:AAHP3V5ELgRSB3MIjaaQ1UZXDiwPcmLyO0Y")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("honey20056")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6659064038")
 
 
 
